37c0aff9f5870e3a66c302ff92285d90bfa65fd1
Ivan Voronov <ivan.voronov.dev@yandex.ru>
2024-06-10 17:08:59 +0500

    [^] APT-23: добавлена проверка авторизации при отправке комментария, запросе на обратную связь с арендодателем

--- src/features/RentApartment/RentDetails/sendRentRequest/ui/RentRequestForm.tsx
+++ src/features/RentApartment/RentDetails/sendRentRequest/ui/RentRequestForm.tsx
@@ -1,12 +1,16 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { memo, useState } from 'react';
+import { memo, useCallback, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import { RentRequestFormSchema, RentRequestFormType } from '../model/types/sendRentRequestTypes';
 import classes from './RentRequestForm.module.scss';
 
 import { Button, Form, TextInput } from '@/shared/ui';
 import { masks } from '@/shared/lib';
+import { getUserData } from '@/entities/User';
+import { ModalQueryValuesEnum, ModalTypeEnum } from '@/shared/types';
 
 enum RentRequestFieldEnum {
   NAME = 'name',
@@ -23,6 +27,10 @@ const defaultFormValues: RentRequestFormType = {
 export const RentRequestForm = memo(function RentRequestFormComponent() {
   const [isLoading, setIsLoading] = useState(false);
   const [isSent, setIsSent] = useState(false);
+
+  const navigate = useNavigate();
+
+  const isAuth = useSelector(getUserData);
 
   const {
     control,
@@ -45,12 +53,25 @@ export const RentRequestForm = memo(function RentRequestFormComponent() {
     setIsSent(true);
   };
 
+  const handleLogIn = useCallback(() => {
+    navigate({ search: `?${ModalTypeEnum.LOGIN}=${ModalQueryValuesEnum.LOGIN}` });
+  }, [navigate]);
+
   if (isSent) {
     return (
       <div className={classes.successMessage}>
         <p>Ваша заявка успешно отправлена!</p>
         <p>Арендодатель свяжется с Вами в ближайшее время.</p>
         <p>Спасибо, что используете наш сервис!</p>
+      </div>
+    );
+  }
+
+  if (!isAuth) {
+    return (
+      <div className={classes.container}>
+        <p>Для того, чтобы связаться с арендодателем, пожалуйста, авторизуйтесь в системе!</p>
+        <Button onClick={handleLogIn}>Авторизоваться</Button>
       </div>
     );
   }
