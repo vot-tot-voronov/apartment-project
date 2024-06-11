94fa1fb765f001b181b3c215836ead2f5daf5ea4
Ivan Voronov <ivan.voronov.dev@yandex.ru>
2024-06-11 09:30:05 +0500

    [^] APT-24: стилизовал страницу 404

--- src/pages/notFound/ui/NotFoundPage.tsx
+++ src/pages/notFound/ui/NotFoundPage.tsx
@@ -1,5 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+
+import classes from './NotFoundPage.module.scss';
+
+import { Button, ButtonThemeEnum, Container } from '@/shared/ui';
+import { getRouteRentList } from '@/shared/config/routeConfig/routeConfig';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+
+  const handleGoBack = () => navigate(getRouteRentList(), { replace: true });
+
+  return (
+    <Container className={classes.container}>
+      <h1 className={classes.title}>404</h1>
+      <p className={classes.text}>Упс, страница, которую вы ищите, не существует.</p>
+      <Button onClick={handleGoBack} className={classes.button} theme={ButtonThemeEnum.SECONDARY}>
+        На главную
+      </Button>
+    </Container>
+  );
 };
 
 export default NotFoundPage;
